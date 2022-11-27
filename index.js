d788fc087081b025f9015ee08d4999eccb2dda4f
x1mukesh <of.mukesh@gmail.com>
2022-11-27 14:42:40 +0530

    update

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 function scrollToTop() {
     window.scrollTo(0, 0)
 }
+
+function resume(){
+    window.open("https://drive.google.com/file/d/1UECJc0DHBhqHtZ7abLkmND0RWfvKHmun/view?usp=sharing")
+}
