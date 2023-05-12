ebab2806908eae1f97dac68a18914582eee8037a
x1mukesh <of.mukesh@gmail.com>
2023-05-12 15:26:56 +0530

    reset

--- index.js
+++
@@ -1,7 +0,0 @@
-function scrollToTop() {
-    window.scrollTo(0, 0)
-}
-
-function resume(){
-    window.open("https://drive.google.com/file/d/1UECJc0DHBhqHtZ7abLkmND0RWfvKHmun/view?usp=sharing")
-}
